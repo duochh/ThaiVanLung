a71427cc0f7d62646031c22441ef69ad68623d9d
duochh <duoc.hh@portcoast.com.vn>
2024-03-19 15:10:45 +0700

    update entrypoint

--- entrypoint.cmd
+++ entrypoint.cmd
@@ -6,11 +6,9 @@
 powershell -ExecutionPolicy Bypass -File "%~dp0.\enable-graphics-apis.ps1"
 
 @rem Run Pixel Streaming package
-echo "Run Pixel Streaming Package"
 powershell "%~dp0.\projects\ThaiVanLung\Packaged\Windows\THAIVANLUNG\Binaries\Win64\THAIVANLUNG.exe -PixelStreamingIP=127.0.0.1 -PixelStreamingPort=8888 -PixelStreamingUrl=ws://localhost:8888 -AllowPixelStreamingCommands -RenderOffScreen -StdOut -FullStdOutLogOutput"
 
 @rem Start Pixel Streaming Signalling server
-echo "Start Pixel Streaming Signalling server"
 powershell "%~dp0.\PixelStreamingInfrastructure\SignallingWebServer\platform_scripts\cmd\Start_SignallingServer.ps1 -Wait -NoNewWindow"
 
 @rem Run the entrypoint command specified via our command-line parameters
